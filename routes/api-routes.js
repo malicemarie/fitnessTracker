16bfe709b68003400c8299af8922cd022a2ce0d6
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-03-29 17:35:29 -0700

    all routes working now. keep forgetting to commit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,17 +2,32 @@
 const router = require("express").Router();
 const Workout = require(`../models/workoutModel`);
 
-router.get(`/api/workouts`, (req, res) => {
+router.post(`/api/workouts`, ({ body }, res) => {
+  Workout.create(body)
+    .then(allExercises => res.json(allExercises))
+    .catch(err => res.status(400).json(err));
+});
+
+router.put(`/api/workouts/:id`, ({ body, params }, res) => {
+  Workout.findOneAndUpdate({ _id: params.id }, { $push: { exercises: body } })
+    .then(allExercises => res.json(allExercises))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.get(`/api/workouts`, res => {
   Workout.find({})
-    .then(dbExercise => {
-      console.log(dbExercise);
-      return res.json(dbExercise);
+    .then(allExercises => {
+      console.log(allExercises);
+      return res.json(allExercises);
     })
     .catch(err => res.status(400).json(err));
 });
-router.get(`/api/workouts/range`, (req, res) => {
+router.get(`/api/workouts/range`, res => {
   Workout.find({})
-    .then(dbExercise => res.json(dbExercise))
+    .then(allExercises => res.json(allExercises))
     .catch(err => res.status(404).json(err));
 });
 
